ae7edbcd0625cec2b672a437b0644214f085bed1
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-04-20 10:30:17 -0500

    can filter through all dates and show only unique dates

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import ShiftsTableView from '@/components/ShiftsTableView'
+import DateSelection from '@/components/DateSelection'
 
 export default function Home() {
   return (
     <main>
+      <DateSelection />
       <ShiftsTableView />
     </main>
   )
--- components/DateSelection.tsx
+++ components/DateSelection.tsx
@@ -2,5 +2,31 @@ import React from 'react'
 import data from '@/lib/data'
 
 export default function DateSelection() {
-  return <div></div>
+  const dateReformat = data.shifts.map((shift) =>
+    shift.date.toLocaleDateString('en-us', {
+      month: 'short',
+      day: 'numeric',
+    })
+  )
+
+  function uniqueDates(date: string[]): string[] {
+    const exists: { [key: string]: boolean } = {}
+    return date.filter((item) => {
+      if (exists.hasOwnProperty(item)) {
+        return false
+      }
+      exists[item] = true
+      return true
+    })
+  }
+
+  const uniqueDatesArray = uniqueDates(dateReformat)
+
+  return (
+    <div>
+      {uniqueDatesArray.map((uniqueDate, i) => (
+        <h3 key={i}>{uniqueDate}</h3>
+      ))}
+    </div>
+  )
 }
